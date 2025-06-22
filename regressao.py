5390074c10ffe86e6261ebe00dd9f2c34af05bd9
Gabriel Akira <gakira.t1@gmail.com>
2025-06-21 21:25:07 -0300

    Plot regressão linear

--- regressao.py
+++ regressao.py
@@ -12,3 +12,17 @@ reg.fit(X,y)
 # %%
 a, b = reg.intercept_, reg.coef_[0]
 print(a, b)
+# %%
+predict =reg.predict(X.drop_duplicates())
+# %%
+from matplotlib import pyplot as plt
+plt.plot(X["cerveja"], y, "o")
+plt.grid(True)
+plt.title("Relação Cerveja vs Nota")
+plt.xlabel("Cerveja")
+plt.ylabel("Nota")
+
+plt.plot(X.drop_duplicates()["cerveja"], predict)
+
+plt.legend(["Observado", f'y = {a :.3f} + {b :.3f} x'])
+# %%
