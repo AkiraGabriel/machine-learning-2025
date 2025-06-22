f81e2256f5dc21f00b7cf737e4152932094ff930
Gabriel Akira <gakira.t1@gmail.com>
2025-06-21 23:12:40 -0300

    Plot de árvore com depth=2

--- regressao.py
+++ regressao.py
@@ -2,7 +2,7 @@
 import pandas as pd
 df = pd.read_excel('data/dados_cerveja_nota.xlsx')
 # %%
-from sklearn import linear_model
+from sklearn import linear_model, tree
 
 X = df[['cerveja']]
 y = df['nota']
@@ -12,17 +12,39 @@ reg.fit(X,y)
 # %%
 a, b = reg.intercept_, reg.coef_[0]
 print(a, b)
-# %%
-predict =reg.predict(X.drop_duplicates())
+
+predict_reg = reg.predict(X.drop_duplicates())
+
+arvore_full = tree.DecisionTreeRegressor(random_state=42)
+arvore_full.fit(X, y)
+predict_arvore_full = arvore_full.predict(X.drop_duplicates())
+
+arvore_d2 = tree.DecisionTreeRegressor(random_state=42,
+                                       max_depth=2)
+arvore_d2.fit(X, y)
+predict_arvore_d2 = arvore_d2.predict(X.drop_duplicates())
 # %%
 from matplotlib import pyplot as plt
+
 plt.plot(X["cerveja"], y, "o")
 plt.grid(True)
 plt.title("Relação Cerveja vs Nota")
 plt.xlabel("Cerveja")
 plt.ylabel("Nota")
 
-plt.plot(X.drop_duplicates()["cerveja"], predict)
+plt.plot(X.drop_duplicates()["cerveja"], predict_reg)
+plt.plot(X.drop_duplicates()['cerveja'], predict_arvore_full, color='red')
+plt.plot(X.drop_duplicates()['cerveja'], predict_arvore_d2, color='purple')
 
-plt.legend(["Observado", f'y = {a :.3f} + {b :.3f} x'])
+plt.legend(["Observado", 
+        f'y = {a :.3f} + {b :.3f} x',
+        'Árvore Full',
+        'Árvore Depth = 2',
+        ])
+
 # %%
+plt.figure(dpi=400)
+
+tree.plot_tree(arvore_d2,
+          feature_names=['cerveja'],
+          filled=True)
